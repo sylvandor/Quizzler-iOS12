fb064aa7da10b55a7e333ac291386a6585740d4b
sylvandor <scottwwj@gmail.com>
2019-04-24 15:55:39 -0500

    Iterate questions on user selection.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     
+    let questions = QuestionBank()
+    
+    var currentQuestionIndex: Int! = 0
+    var correctAnswers = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,31 +25,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        let userAnswer = sender.tag == 1 ? true : false
+        
+        checkAnswer(userAnswer)
+       
+        nextQuestion()
+        updateUI()
     }
     
     
     func updateUI() {
-      
+//        progressBar
+        
+        let question = questions.list[currentQuestionIndex]
+        
+        questionLabel.text = question.questionText
     }
     
 
     func nextQuestion() {
-        
+        currentQuestionIndex = currentQuestionIndex < questions.list.count - 1 ?  currentQuestionIndex + 1 : 0
     }
     
     
-    func checkAnswer() {
-        
+    func checkAnswer(_ userAnswer: Bool) {
+        let question = questions.list[currentQuestionIndex]
+        if userAnswer == question.answer {
+            print("Correct")
+        } else {
+            print("Incorrect")
+        }
     }
     
     
     func startOver() {
-       
+   
     }
     
 
